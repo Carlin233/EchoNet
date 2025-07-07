eec76a6c2455c7f3ebaa3b742a3d0ff026ce342e
Carlos Henrique <carlindiv001@gmail.com>
2025-07-07 17:25:31 -0300

    BARRA DE CONTATOS COLOCADA COM SUCESSO

--- server.js
+++ server.js
@@ -269,3 +269,33 @@ app.post("/mensagens", (req, res) => {
     }
   );
 });
+
+app.post("/atualizar-ativo", (req, res) => {
+  const usuario = req.session.usuario;
+  if (!usuario) return res.sendStatus(401);
+
+  const query = `UPDATE users SET ultimo_ativo = datetime('now') WHERE username = ?`;
+  db.run(query, [usuario], (err) => {
+    if (err) return res.sendStatus(500);
+    res.sendStatus(200);
+  });
+});
+
+app.get("/online-users", (req, res) => {
+  const query = `
+    SELECT username, 
+      CASE
+        WHEN ultimo_ativo >= datetime('now', '-1 minute') THEN 'online'
+        WHEN ultimo_ativo >= datetime('now', '-5 minutes') THEN 'away'
+        ELSE 'offline'
+      END as status
+    FROM users
+    WHERE username != ?
+    ORDER BY username;
+  `;
+
+  db.all(query, [req.session.usuario], (err, rows) => {
+    if (err) return res.sendStatus(500);
+    res.json(rows);
+  });
+});
